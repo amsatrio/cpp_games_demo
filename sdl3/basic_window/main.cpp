8980ac0ccc8a40256c89eb80b5743b473cfb092a
amsatrio <79563756+amsatrio@users.noreply.github.com>
2026-03-20 13:14:32 +0700

    update basic window sdl3

--- sdl3/basic_window/main.cpp
+++ sdl3/basic_window/main.cpp
@@ -1,25 +1,53 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
 #include <SDL3/SDL_main.h>
 
-typedef struct {
+struct SDLState {
     SDL_Renderer *renderer;
     SDL_Window *window;
-} App;
+};
 
-App app;
+SDLState state;
 
-int sdl_init() {
-    // SDL3 uses a more unified initialization
+void cleanup(SDLState &state);
+
+int init();
+
+int main(int argc, char* argv[]) {
+    if (init() == 1) return 1;
+
+    bool quit = false;
+    SDL_Event event{0};
+
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                quit = true;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(state.renderer, 33, 33, 33, 255);
+        SDL_RenderClear(state.renderer);
+
+        SDL_RenderPresent(state.renderer);
+    }
+
+    cleanup(state);
+
+    return 0;
+}
+
+int init() {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         return 1;
     }
 
-    // SDL_CreateWindow no longer needs position parameters unless you specifically want them
-    // You can also chain the renderer creation or use the new combined call
     const int screen_width = 800;
     const int screen_height = 480;
 
-    if (!SDL_CreateWindowAndRenderer("SDL3 Basic Window", screen_width, screen_height, 0, &app.window, &app.renderer)) {
+    if (!SDL_CreateWindowAndRenderer("SDL3 Basic Window", screen_width, screen_height, 0, &state.window, &state.renderer)) {
         SDL_Quit();
         return 1;
     }
@@ -27,31 +55,8 @@ int sdl_init() {
     return 0;
 }
 
-int main(int argc, char* argv[]) {
-    if (sdl_init() == 1) return 1;
-
-    bool quit = false;
-    SDL_Event event;
-
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                quit = true;
-            }
-        }
-
-        // Draw a dark grey background
-        SDL_SetRenderDrawColor(app.renderer, 33, 33, 33, 255);
-        SDL_RenderClear(app.renderer);
-
-        // Update screen
-        SDL_RenderPresent(app.renderer);
-    }
-
-    // Clean up
-    SDL_DestroyRenderer(app.renderer);
-    SDL_DestroyWindow(app.window);
+void cleanup(SDLState &state) {
+    SDL_DestroyRenderer(state.renderer);
+    SDL_DestroyWindow(state.window);
     SDL_Quit();
-
-    return 0;
 }
